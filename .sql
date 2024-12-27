6710a563bbdfbca4b7a4860fffd2051595455cea
Cherry2703 <ramcharanamr2408@gmail.com>
2024-12-27 11:51:00 +0530

    first commit

---
+++ .sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE  travel (
+--                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+--                 name TEXT NOT NULL,
+--                 mobile TEXT NOT NULL UNIQUE,
+--                 boarding TEXT NOT NULL,
+--                 destination TEXT NOT NULL,
+--                 email TEXT NOT NULL UNIQUE,
+--                 date TEXT NOT NULL 
+--             );
---
+++ server.js
@@ -0,0 +1,44 @@
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const { open } = require('sqlite');
+const sqlite3 = require('sqlite3');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const dbPath = path.join(__dirname, './database.db');
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+let db;
+
+// Initialize database and server
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}/`);
+    });
+  } catch (error) {
+    console.error('Failed to start the server:', error);
+    process.exit(1); // Exit the process with failure code
+  }
+};
+
+initializeDBAndServer();
+
+
+
+
+app.get("/travel/:id", async(req, res) => {
+    const { id } = req.params;
+    const query = `SELECT * FROM travel WHERE id = ${id}`;
+    const data = await db.get(query)
+    res.status(200).send(data)
+});
